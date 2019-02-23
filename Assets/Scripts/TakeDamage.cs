66db9f2539de58961887686ce9fc798175ff2566
AlessioBona <alefbona@gmail.com>
2019-02-23 19:45:42 +0100

    TakeDamage script added

---
+++ Assets/Scripts/TakeDamage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeDamage : MonoBehaviour
+{
+    public int health = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Organism")
+        {
+            GetDamaged(collision.gameObject);
+        }
+    }
+
+    private void GetDamaged(GameObject organism)
+    {
+        Debug.Log(organism.tag);
+    }
+}
