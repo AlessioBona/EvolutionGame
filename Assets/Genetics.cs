a9f458dcaa520659c9db1c50248607d5a799a5e1
AlessioBona <alefbona@gmail.com>
2019-02-23 18:04:30 +0100

    Genetics

---
+++ Assets/Genetics.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Genetics : MonoBehaviour
+{
+    public float speed;
+    public float directionAngle;
+    public float hue;
+
+    public void Mutation()
+    {
+        Debug.Log("I mutate");
+        speed++;
+        // hier should we mutate the variables
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Procreation.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Procreation : MonoBehaviour
+{
+
+
+    public bool toggleOn = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Procreate()
+    {
+        
+        GameObject firstChild = Instantiate(gameObject);
+        firstChild.GetComponent<Genetics>().Mutation();
+    }
+
+    
+
+    private void OnMouseDown()
+    {
+        toggleOn = !toggleOn;
+        if (GetComponentInChildren<TextMeshProUGUI>())
+        {
+            TextMeshProUGUI togOn = GetComponentInChildren<TextMeshProUGUI>();
+            togOn.enabled = !togOn.enabled;
+        }
+
+        Procreate();
+
+        
+    }
+
+}
